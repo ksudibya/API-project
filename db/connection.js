1ef9c5aaf546dd263e28a78c1d12ebb455475568
ksudibya <ksudibya@gmail.com>
2023-04-12 11:12:59 -0400

    success1

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
-const MONGODB_URI = process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/beer-api"
+const MONGODB_URI = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/beer-api"
 
 // This is for Model.findByIDAndUpdate method, specifically so that {new: true} is the default
 mongoose.set("returnOriginal", false);
